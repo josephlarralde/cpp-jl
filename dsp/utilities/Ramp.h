944772b86c6cf58a0d4448fd5796d96d6ad47ca2
Joseph <joseph.larralde@gmail.com>
2018-08-18 16:37:35 +0200

    added utilities

---
+++ dsp/utilities/Ramp.h
@@ -0,0 +1,112 @@
+/**
+ * @file Ramp.h
+ * @author Joseph Larralde
+ * @date 01/08/2018
+ * @brief all-purpose audio ramp generator
+ *
+ * @copyright
+ * Copyright (C) 2018 by Joseph Larralde.
+ * All rights reserved.
+ *
+ * License (BSD 3-clause)
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _JL_RAMP_H_
+#define _JL_RAMP_H_
+
+namespace jl {
+
+// super basic linear ramping class, working in sample units
+
+// see https://dsp.stackexchange.com/questions/14754/equal-power-crossfade/49989#49989
+// (a very detailed post from robert bristow-johnson about cross-fading)
+
+// some say on music-dsp that half a cosine period is as fine as a linear ramp
+// TODO : compare both kinds of fade and choose by ear
+
+// the Gbend class could be rewritten using mostly ramps (for fades, interrupt and buffer index)
+// TODO : create a speed factor driven version of the ramp (?)
+
+template <typename TI, typename TO>
+class Ramp {
+private:
+  TO value;
+  TO target;
+  TO *block;//[JL_MAX_BLOCK_SIZE];
+  // std::vector<TO> block;
+  unsigned int bSize;
+  bool ramping;
+  TO increment;
+  unsigned long sRemaining;
+
+public:
+  Ramp(unsigned int blockSize = 64) : bSize(blockSize) {
+    block = new TO[bSize];
+  }
+  ~Ramp() {
+    delete[] block;
+  }
+
+  void ramp(TI t, unsigned long samples) {
+    target = static_cast<TO>(t);
+    increment = (t - value) / samples;
+    ramping = true;
+    sRemaining = samples;
+  }
+
+  TO *process(unsigned int blockSize) {
+    if (blockSize != bSize) {
+      delete[] block;
+      bSize = blockSize;
+      block = new TO[bSize];
+    }
+
+    TO *out = &(block[0]);
+
+    for (unsigned int i = 0; i < blockSize; ++i) {
+      if (ramping) {
+        if (sRemaining == 0) {
+          ramping = false;
+          value = target;
+          *out++ = value;
+        } else {
+          value += increment;
+          *out++ = value;
+          sRemaining--;
+        }
+      } else {
+        *out++ = value;
+      }
+    }
+
+    return &(block[0]);
+  }
+};
+
+} /* end namespace jl */
+
+#endif /* _JL_RAMP_H_ */
---
+++ dsp/utilities/units.h
@@ -0,0 +1,90 @@
+/**
+ * @file units.h
+ * @author Joseph Larralde
+ * @date 01/08/2018
+ * @brief audio related unit converters
+ *
+ * @copyright
+ * Copyright (C) 2018 by Joseph Larralde.
+ * All rights reserved.
+ *
+ * License (BSD 3-clause)
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _JL_UNITS_H_
+#define _JL_UNITS_H_
+
+#include <cmath>
+#include <cassert>
+#include <limits>
+#include "../../jl.h"
+
+namespace jl {
+
+// source :
+// http://www.sengpielaudio.com/calculator-FactorRatioLevelDecibel.htm
+
+// amplitude 1 <=> 0 dB
+
+static_assert(std::numeric_limits<float>::is_iec559, "IEEE 754 required");
+static_assert(std::numeric_limits<double>::is_iec559, "IEEE 754 required");
+
+template <typename T>
+T clipInfinity(T val) {
+  if (val == -std::numeric_limits<T>::infinity()) {
+    return std::numeric_limits<T>::lowest();
+  } else if (val == std::numeric_limits<T>::infinity()) {
+    return std::numeric_limits<T>::max();
+  }
+
+  return val;
+}
+
+template <typename T>
+T dbtoa(T db) {
+  T realdb = db;
+  if (db == -std::numeric_limits<T>::infinity()) {
+    realdb = std::numeric_limits<T>::lowest();
+  }
+
+  return pow(10, realdb / 20);
+}
+
+template <typename T>
+T atodb(T a) {
+  T res = 20 * log10(a);
+
+  if (res == -std::numeric_limits<T>::infinity()) {
+    return std::numeric_limits<T>::lowest();
+  }
+
+  return res;
+}
+
+} /* end namespace jl */
+
+#endif /* _JL_UNITS_H_ */
